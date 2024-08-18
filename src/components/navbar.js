b0badfe0875808073e36563030a8de0dd141174f
sanad <sanadbinsayedsanu@gmail.com>
2024-08-18 12:52:06 +0530

    added a fully working navbar styling remaing

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './navbar.css';
+
+const Navbar = ()=>{
+    const[ open , setopen]=useState(false);
+
+    const toggle=()=>{
+        setopen(!open);
+    }
+    return(
+        <nav className='navbar'>
+            <div className='brand'>
+                {/* <a href='#'><img alt='logo' src='' className='logo_image'/></a> */}
+                <a href='#' className='brandName'>twu</a>
+                
+            </div>
+            <div className={`menuItems ${open ? 'open' : ''}`}>
+                <a href='#'>Home</a>
+                <a href='#'>About</a>
+                <a href='#'>Projects</a>
+            </div>
+            <button className='toggler' onClick={toggle}>☰</button>
+        </nav>
+    )
+}
+export default Navbar;

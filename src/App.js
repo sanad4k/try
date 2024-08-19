eeaa9c2548ea22b6d351a41c8eb57354c8af3d08
sanad <sanadbinsayedsanu@gmail.com>
2024-08-20 01:15:17 +0530

    working card is available

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Navbar from './components/navbar'
 import Card from './components/card'
+// import car from'./photos/senna.jpg'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className='parent'>
-        <Card src='../p/senna.jpg' />
+        <Card src='/photos/senna.jpg' />
       </div>
       <h1>hey is it up there</h1>
-
-
+      {/* <img src={car} alt='car here man'/> */}
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,21 @@
-const Card =({src})=>{
-    return(
+import React from "react";
+import './card.css'
+const Card = ({ src }) => {
+    // const src = {src}
+    return (
         <div className="card">
             <div className="image-container">
-                <img className="the_image" alt="project_image" src={src}></img>
+                
+                
+                <img className="the_image" alt="project_image" src="/photos/senna.jpg"/>
             </div>
             <div className="text_box">
                 <div className="title">SENNA</div>
                 <div className="title">UPRA SAE India 2023, Overall AIR 27 </div>
             </div>
+            <img className="the_image" alt="project_image" src={src}/>
         </div>
+        
     )
 }
 export default Card

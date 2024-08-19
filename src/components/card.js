e24949a6c3a2ebaee337c69d7af9681c640a813e
sanad <sanadbinsayedsanu@gmail.com>
2024-08-20 01:36:12 +0530

    card not added?

--- src/components/card.js
+++ src/components/card.js
@@ -5,15 +5,12 @@ const Card = ({ src }) => {
     return (
         <div className="card">
             <div className="image-container">
-                
-                
                 <img className="the_image" alt="project_image" src="/photos/senna.jpg"/>
             </div>
             <div className="text_box">
                 <div className="title">SENNA</div>
                 <div className="title">UPRA SAE India 2023, Overall AIR 27 </div>
             </div>
-            <img className="the_image" alt="project_image" src={src}/>
         </div>
         
     )

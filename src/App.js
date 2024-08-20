4cf5e42a326cc391bc99704751deb745b8fbdb0d
sanad <sanadbinsayedsanu@gmail.com>
2024-08-20 19:59:35 +0530

    well made some changes nothing broke yet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='title-1'></div>
       <div className='parent'>
+        <Card src='/photos/senna.jpg' />
+        <Card src='/photos/senna.jpg' />
+        <Card src='/photos/senna.jpg' />
         <Card src='/photos/senna.jpg' />
       </div>
       <h1>hey is it up there</h1>

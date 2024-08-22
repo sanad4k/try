b22c17cfe20020ce11264ce2e3f684d4ea7c8913
sanad <sanadbinsayedsanu@gmail.com>
2024-08-22 19:23:00 +0530

    added a decent footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar'
 import Card from './components/card'
+import Footer from './components/footer';
 // import car from'./photos/senna.jpg'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className='title-1'></div>
+      <div className='title-1'>projects</div>
       <div className='parent'>
         <Card src='/photos/senna.jpg' />
         <Card src='/photos/senna.jpg' />
@@ -15,7 +16,7 @@ function App() {
         <Card src='/photos/senna.jpg' />
       </div>
       <h1>hey is it up there</h1>
-      {/* <img src={car} alt='car here man'/> */}
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section">
+          <h3>For Enquiries Contact</h3>
+          <p>Dr.V Sajith, Associate Professor (MED) & Faculty Advisor</p>
+          <p>Team Unwired, NIT Calicut</p>
+          <p>Phone: +91 9446361125</p>
+          <p>Email: <a href="mailto:sajith@nitc.ac.in">sajith@nitc.ac.in</a></p>
+        </div>
+        <div className="footer-section">
+          <h3>For More Information</h3>
+          <p>Dr.Shijo Thomas, Associate Professor (MED) & Faculty Advisor</p>
+          <p>Team Unwired, NIT Calicut</p>
+          <p>Phone: +91 9446276409</p>
+          <p>Email: <a href="mailto:shijo@nitc.ac.in">shijo@nitc.ac.in</a></p>
+        </div>
+        <div className="footer-section">
+          <h3>Contact Address</h3>
+          <p>Team Unwired</p>
+          <p>C/o Dr.V Sajith / Dr.Shijo Thomas</p>
+          <p>Department of Materials Science and Engineering</p>
+          <p>National Institute of Technology Calicut</p>
+          <p>Kattangal, Kerala, India</p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

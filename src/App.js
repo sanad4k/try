61181e008c94b7c67ef8e3744cab12b49e6536bd
sanad <sanadbinsayedsanu@gmail.com>
2024-09-02 02:10:57 +0530

    made some changes to card

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function App() {
     <div className="App">
       <Navbar />
       <div className='title-1'>projects</div>
-      <div className='parent'>
-        <Card src='/photos/senna.jpg' />
-        <Card src='/photos/senna.jpg' />
-        <Card src='/photos/senna.jpg' />
-        <Card src='/photos/senna.jpg' />
+      <div className='center'>
+        <div className='parent'>
+          <Card src='/photos/senna.jpg' name='senna' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration ' />
+          <Card src='/photos/minnal.jpg' />
+          <Card src='/photos/TEQUIP.jpg' />
+          <Card src='/photos/unleashed.jpg' />
+          <Card src='/photos/ZEUS.jpg' />
+        </div>
       </div>
       <h1>hey is it up there</h1>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,17 @@
 import React from "react";
 import './card.css'
-const Card = ({ src }) => {
+const Card = ({ src , name , details1 , details2}) => {
     // const src = {src}
     return (
         <div className="card">
             <div className="image-container">
-                <img className="the_image" alt="project_image" src="/photos/senna.jpg"/>
+                <img className="the_image" alt="project_image" src={src}/>
             </div>
             <div className="text_box">
-                <div className="title">SENNA</div>
-                <div className="title">UPRA SAE India 2023, Overall AIR 27 </div>
+                <div className="title">{name}</div>
+                <div className="title details1">{details1}</div>
+                <div className="title details2">{details2}</div>
+                <br/>   
             </div>
         </div>
         

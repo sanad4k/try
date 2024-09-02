fad4defc2a8c016f13f092839f30d2c5f40fe3fc
sanad <sanadbinsayedsanu@gmail.com>
2024-09-02 19:36:45 +0530

    card and title

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className='title-1'>projects</div>
+      <h1 className='title_p'>WITH PRIDE WE PRESENT</h1>
+      
       <div className='center'>
         <div className='parent'>
           <Card src='/photos/senna.jpg' name='SENNA' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration ' />
-          <Card src='/photos/minnal.jpg' />
-          <Card src='/photos/TEQUIP.jpg' />
-          <Card src='/photos/unleashed.jpg' />
-          <Card src='/photos/ZEUS.jpg' />
+          <Card src='/photos/minnal.jpg' name='MINNAL' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration ' />
+          <Card src='/photos/TEQUIP.jpg' name='TEQUIP' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration ' />
+          <Card src='/photos/unleashed.jpg' name='UNLEASHED' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration ' />
+          <Card src='/photos/ZEUS.jpg'name='ZEUS' details1='SUPRA SAE India 2023| Overall AIR 27' details2='Bajaj Dominar 400 engine Suspension: Front - Pull rod, Rear-Push rod Hanging Chassis ASI 4130 Chromoly Chassis 7th in design 10th in acceleration '  />
         </div>
       </div>
       <h1>hey is it up there</h1>
